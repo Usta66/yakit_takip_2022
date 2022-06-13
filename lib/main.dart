1364fd683a1a0eb917d3f3ad6723998c72a331ba
Usta66 <65877212+Usta66@users.noreply.github.com>
2022-06-13 23:11:07 +0300

    admob start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:yakit_takip_2022/constants/app_constants.dart';
 
 import 'package:yakit_takip_2022/navigation/navigation_services.dart';
@@ -7,9 +8,14 @@ import 'package:yakit_takip_2022/navigation/navigation_services.dart';
 import 'navigation/navigation_route_services.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(EasyLocalization(
       startLocale: const Locale("tr", "TR"),
-      supportedLocales: const [LocaleConstants.EN_LOCALE, LocaleConstants.TR_LOCALE],
+      supportedLocales: const [
+        LocaleConstants.EN_LOCALE,
+        LocaleConstants.TR_LOCALE
+      ],
       path: LocaleConstants.LANG_PATH,
       child: const MyApp()));
 }
@@ -25,11 +31,16 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       title: 'Yakıt Takip',
-      theme: ThemeData(useMaterial3: true, primarySwatch: Colors.amber, textTheme: const TextTheme(subtitle2: TextStyle(color: Colors.amber))),
+      theme: ThemeData(
+          useMaterial3: true,
+          primarySwatch: Colors.amber,
+          textTheme:
+              const TextTheme(subtitle2: TextStyle(color: Colors.amber))),
       initialRoute: "aracListesi",
       navigatorKey: NavigationServices.instance.navigatorKey,
 
-      onGenerateRoute: (settings) => NavigatorRouteServices.onRouteGenarete(settings),
+      onGenerateRoute: (settings) =>
+          NavigatorRouteServices.onRouteGenarete(settings),
 
       //home: MyHomePage(),
     );
